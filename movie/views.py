f3d32dcefc752c6ae6ae87411f77194e20af4643
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2023-08-03 10:59:05 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page<h1>')
-    return render(request, 'home.html', {'name': 'Luisa María'})
+    return render(request, 'home.html', {'name': 'Luisa María Alvarez'})
 # Create your views here.
