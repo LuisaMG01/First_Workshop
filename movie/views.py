4d48844930d732a47243859ac5d3b4f2d67c1c9c
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2023-08-09 11:30:13 -0500

    Formulario de busqueda

--- movie/views.py
+++ movie/views.py
@@ -5,11 +5,8 @@ from .models import movie
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page<h1>')
     searchTerm = request.GET.get('searchMovie')
-    if searchTerm:
-        movies = movie.objects.filter(title__icontains = searchTerm)
-    else: 
-        movies = movie.objects.all()
-    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+    
+    return render(request, 'home.html', {'searchTerm': searchTerm})
 # Create your views here.
 
 
