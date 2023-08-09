11361c2186e63e7abb7a977e4e60723d7c12ddfd
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2023-08-09 11:23:17 -0500

    Visualizacion de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import movie
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page<h1>')
-    return render(request, 'home.html', {'name': 'Luisa María Alvarez'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = movie.objects.filter(title__icontains = searchTerm)
+    else: 
+        movies = movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 # Create your views here.
+
+
+def about(request):
+    return render(request, 'about.html', {'name': 'Luisa María Alvarez'})
